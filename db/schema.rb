938fd27f3ae2e75ba6b8c6e0ef439727d7f6bf8f
tsumu812 <sfpe.jwax8@gmail.com>
2020-04-25 01:45:47 +0900

    ユーザー画像アップロード機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_142052) do
+ActiveRecord::Schema.define(version: 2020_04_24_144522) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "shop_id"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_04_16_142052) do
     t.datetime "updated_at", null: false
     t.text "introduction"
     t.boolean "admin", default: false
+    t.string "picture"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
